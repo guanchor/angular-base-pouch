145286b4bfac19e706ef31284e9e2147ac6a54c2
Guanchor Ojeda <guanchor.ojeda@hyve.net>
2018-02-11 22:47:49 +0000

    [user] Upgraded User page

--- app/pages/user/user.controller.js
+++ app/pages/user/user.controller.js
@@ -5,16 +5,59 @@
     .module('edata')
     .controller('UserController', UserController);
 
-  UserController.$inject = ['$scope'];
+  UserController.$inject = ['$scope', 'userService'];
 
-  function UserController($scope)
+  function UserController($scope, userService)
   {
     var vm = this;
-    var TAG = 'UserController';
+    vm.reset = reset;
+    vm.submit = submit;
+    vm.validateField = validateField;
 
     initialization();
 
-    function initialization() {}
+    function initialization() {
+      vm.expireDate = new Date();
+      vm.firstName = 'Guanchor';
+      vm.lastName = 'Ojeda';
+      vm.status = 'active';
+    }
+
+    function reset() {
+      initialization();
+    }
+
+    function validateField(field) {
+      userService.validateField(field, vm[field]);
+      updateErrorMessages();
+    }
+
+    function updateErrorMessages () {
+      let errors = userService.getErrors();
+
+      Object.keys( errors ).forEach( label  => {
+        if ($scope.userForm.hasOwnProperty(label)) {
+          Object.keys( errors[label] ).forEach( rule  => {
+            errors[label][rule] ? null : console.error(label, rule, errors[label][rule]);
+            $scope.userForm[label].$setValidity('custom-' + rule, errors[label][rule]);
+          });
+        }
+        else {
+          $scope.userForm.$invalid = true;
+        }
+      });
+    }
+
+    function submit() {
+      userService.save(vm)
+        .catch(updateErrorMessages)
+        .then(redirect);
+    }
+
+    function redirect (response) {
+      console.log(response);
+      $state.go("users");
+    }
   }
 
 })();
---
+++ app/pages/user/user.service.js
@@ -0,0 +1,119 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('edata')
+    .factory('userService', userService);
+
+  userService.$inject = ['$window', '$q'];
+
+  function userService($window, $q) {
+    var _errors = [],
+      _rules = {
+        expireDate: { 'required': null },
+        firstName: { 'required': null, 'maxLength': 100 },
+        lastName: { 'required': null, 'maxLength': 100 },
+        status: { 'required': null },
+      },
+      _setError = function(field, rule, status) {
+        if (!_errors[field]) {
+          _errors[field] = [];
+        }
+        return _errors[field][rule] = status;
+      },
+      _validators = {
+        required: function(value) {
+          return value !== null && value !== "" && value !== [];
+        },
+        maxLength: function(value, length) {
+          return (typeof value === 'string' || value instanceof String) && value.length <= length;
+        }
+      }
+    ;
+
+    // services return
+    var service = {
+      getErrors: getErrors,
+      save: save,
+      validateField: validateField
+    };
+    return service;
+    //
+
+    function getErrors() {
+      return _errors;
+    }
+
+    function save(data) {
+      var doc = {},
+        db = $window.PouchDB('edata_db')
+      ;
+
+      saveBind(data);
+      try {
+        saveValidation();
+      }
+      catch (e) {
+        return $q.reject(e);
+      }
+
+      return db.post(doc)
+        //.catch(saveError)
+        //.then(saveComplete)
+      ;
+
+      function saveBind(data) {
+        doc.expireDate = data.expireDate;
+        doc.firstName = data.firstName;
+        doc.lastName = data.lastName;
+        doc.status = data.status;
+      }
+
+      function saveValidation() {
+        var valid = true;
+
+        Object.keys( _rules ).forEach( field => {
+          if (!validateField(field, doc[field]) && valid) {
+            valid = false;
+          }
+        });
+
+        if (!valid) {
+          throw 'Validation Error';
+        }
+        return valid;
+      }
+
+      function saveError(error) {
+        return false;
+      }
+
+      function saveComplete(response) {
+        return db.get(response.id);
+      }
+    }
+
+    function validateField(field, value) {
+      let fieldRules = _rules[field],
+        valid = true,
+        validationParams = {}
+      ;
+
+      Object.keys( fieldRules ).forEach( rule => {
+        validationParams = [ value ].concat( fieldRules[rule] );
+
+        if (_validators[rule].apply( null, validationParams )) {
+          _setError(field, rule, true);
+        }
+        else {
+          valid = false;
+          _setError(field, rule, false);
+        }
+      });
+
+      return valid;
+    }
+
+  }
+
+})();
