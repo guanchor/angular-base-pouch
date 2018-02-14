d2bd271a705955eb7358ec10127681a6b2192532
Guanchor Ojeda <guanchor.ojeda@hyve.net>
2018-02-14 09:18:29 +0000

    [app] Edata module cleaned

--- app/edata.module.js
+++ app/edata.module.js
@@ -11,82 +11,21 @@
     .module('edata', [
       'pouchdb',
       'ngRoute',
-      'angularjs-datetime-picker'
+      'angularjs-datetime-picker',
+      'angular-md5'
     ])
     .directive('topHeader', function() {
       return {
         restrict: 'E',
-        templateUrl: './app/directives/top-header.view.html'
+        templateUrl: './app/top-header/top-header.view.html'
       };
     })
     .directive('sideMenu', function() {
       return {
         restrict: 'E',
-        templateUrl: './app/directives/side-menu.view.html'
+        templateUrl: './app/side-menu/side-menu.view.html'
       };
     })
-    .service('pouchdbService', pouchdbService)
   ;
 
-  function pouchdbService($q) {
-    var database;
-    var changeListener;
-
-    initialization();
-
-    function initialization() {
-      database = new PouchDB('edata_db');
-    }
-
-    this.startListening = function() {
-      changeListener = database.changes({
-        live: true,
-        include_docs: true
-      }).on("change", function(change) {
-        if(!change.deleted) {
-          $rootScope.$broadcast("$pouchDB:change", change);
-        } else {
-          $rootScope.$broadcast("$pouchDB:delete", change);
-        }
-      });
-    }
-
-    this.stopListening = function() {
-      changeListener.cancel();
-    }
-
-    this.sync = function(remoteDatabase) {
-      database.sync(remoteDatabase, {live: true, retry: true});
-    }
-
-    this.save = function(jsonDocument) {
-      var deferred = $q.defer();
-      if(!jsonDocument._id) {
-        database.post(jsonDocument).then(function(response) {
-          deferred.resolve(response);
-        }).catch(function(error) {
-          deferred.reject(error);
-        });
-      } else {
-        database.put(jsonDocument).then(function(response) {
-          deferred.resolve(response);
-        }).catch(function(error) {
-          deferred.reject(error);
-        });
-      }
-      return deferred.promise;
-    }
-
-    this.delete = function(documentId, documentRevision) {
-      return database.remove(documentId, documentRevision);
-    }
-
-    this.get = function(documentId) {
-      return database.get(documentId);
-    }
-
-    this.destroy = function() {
-      database.destroy();
-    }
-  }
 })();
